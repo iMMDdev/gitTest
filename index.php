b75edfec2719b44f35a190b9b643e0843eba752b
iMMDdev <mamadho2019@gmail.com>
2023-01-30 00:42:28 +0330

    add project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index page</title>
+</head>
+<body>
+<h1>welcome to index page</h1>
+<h2>index page index</h2>
+<span>lorem ipsum</span>
+<span>lorem ipsum</span>
+<span>lorem ipsum</span>
+<span>lorem ipsum</span>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, animi aspernatur assumenda commodi esse ex impedit in ipsam iste laborum laudantium libero molestias non numquam saepe similique sint tempore voluptatibus?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, animi aspernatur assumenda commodi esse ex impedit in ipsam iste laborum laudantium libero molestias non numquam saepe similique sint tempore voluptatibus?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, animi aspernatur assumenda commodi esse ex impedit in ipsam iste laborum laudantium libero molestias non numquam saepe similique sint tempore voluptatibus?</p>
+</body>
+</html>
